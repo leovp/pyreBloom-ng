df47535a66cdbd5c2d2131ba9802a3521a7410c6
leovp <leovp@users.noreply.github.com>
2016-08-03 16:19:03 +0300

    Updated tests to reflect the latest changes

--- test/test_accuracy.py
+++ test/test_accuracy.py
@@ -12,13 +12,13 @@ from pyreBloom import PyreBloom, PyreBloomException
 def sample_strings(length, count):
     """Return a set of sample strings"""
     return [''.join(
-        random.sample(string.ascii_lowercase, length)) for _ in range(count)]
+        random.sample(string.ascii_lowercase, length)).encode('utf-8') for _ in range(count)]
 
 
 class BaseTest(unittest.TestCase):
     CAPACITY = 10000
     ERROR_RATE = 0.1
-    KEY = 'pyreBloomTesting'
+    KEY = b'pyreBloomTesting'
 
     def setUp(self):
         self.bloom = PyreBloom(self.KEY, self.CAPACITY, self.ERROR_RATE)
@@ -38,7 +38,7 @@ class ErrorsTest(BaseTest):
         """In this test I want to make sure that we can catch errors when
         connecting to a redis instance"""
         self.assertRaises(PyreBloomException, PyreBloom,
-                          'pyreBloomTesting', 100, 0.01, port=1234)
+                          b'pyreBloomTesting', 100, 0.01, port=1234)
 
     def test_error(self):
         """If we encounter a redis error, we should raise exceptions"""
@@ -46,53 +46,53 @@ class ErrorsTest(BaseTest):
         # If it's the wrong key type, we should see errors. Specifically, this has one
         # of the keys used as a hash instead of a string.
         self.redis.hmset('pyreBloomTesting.0', {'hello': 5})
-        self.assertRaises(PyreBloomException, self.bloom.add, 'hello')
-        self.assertRaises(PyreBloomException, self.bloom.extend, ['a', 'b'])
-        self.assertRaises(PyreBloomException, self.bloom.contains, 'a')
-        self.assertRaises(PyreBloomException, self.bloom.contains, ['a', 'b'])
+        self.assertRaises(PyreBloomException, self.bloom.add, b'hello')
+        self.assertRaises(PyreBloomException, self.bloom.update, [b'a', b'b'])
+        self.assertRaises(PyreBloomException, self.bloom.contains, b'a')
+        self.assertRaises(PyreBloomException, self.bloom.intersection, [b'a', b'b'])
 
 
 class FunctionalityTest(BaseTest):
     def test_delete(self):
         """Make sure that when we delete the bloom filter, we really do"""
         samples = sample_strings(20, 5000)
-        self.bloom.extend(samples)
+        self.bloom.update(samples)
         self.bloom.delete()
-        self.assertEqual(len(self.bloom.contains(samples)), 0,
+        self.assertEqual(len(self.bloom.intersection(samples)), 0,
                          'Failed to actually delete filter')
 
     def test_add(self):
         """Make sure we can add, check existing in a basic way"""
-        tests = ['hello', 'how', 'are', 'you', 'today']
+        tests = [b'hello', b'how', b'are', b'you', b'today']
         for test in tests:
             self.bloom.add(test)
         for test in tests:
             self.assertTrue(test in self.bloom)
 
-    def test_extend(self):
+    def test_update(self):
         """Make sure we can use the extend method to the same effect"""
-        tests = ['hello', 'how', 'are', 'you', 'today']
-        self.bloom.extend(tests)
+        tests = [b'hello', b'how', b'are', b'you', b'today']
+        self.bloom.update(tests)
         for test in tests:
             self.assertTrue(test in self.bloom)
 
-    def test_contains(self):
+    def test_intersection(self):
         """Make sure contains returns a list when given a list"""
-        tests = ['hello', 'how', 'are', 'you', 'today']
-        self.bloom.extend(tests)
-        self.assertEqual(tests, self.bloom.contains(tests))
+        tests = [b'hello', b'how', b'are', b'you', b'today']
+        self.bloom.update(tests)
+        self.assertEqual(tests, self.bloom.intersection(tests))
 
     def test_two_instances(self):
         """Make sure two bloom filters pointing to the same key work"""
-        bloom = PyreBloom('pyreBloomTesting', 10000, 0.1)
-        tests = ['hello', 'how', 'are', 'you', 'today']
+        bloom = PyreBloom(b'pyreBloomTesting', 10000, 0.1)
+        tests = [b'hello', b'how', b'are', b'you', b'today']
 
         # Add them through the first instance
-        self.bloom.extend(tests)
-        self.assertEqual(tests, self.bloom.contains(tests))
+        self.bloom.update(tests)
+        self.assertEqual(tests, self.bloom.intersection(tests))
 
         # Make sure they're accessible through the second instance
-        self.assertEqual(tests, bloom.contains(tests))
+        self.assertEqual(tests, bloom.intersection(tests))
 
 
 class DbTest(BaseTest):
@@ -104,8 +104,8 @@ class DbTest(BaseTest):
 
         # After adding key to our db=0 bloom filter, shouldn't see it in our db=0 bloom
         samples = sample_strings(20, 100)
-        self.bloom.extend(samples)
-        self.assertEqual(len(bloom.contains(samples)), 0)
+        self.bloom.update(samples)
+        self.assertEqual(len(bloom.intersection(samples)), 0)
 
 
 class AllocationTest(BaseTest):
@@ -120,17 +120,18 @@ class AllocationTest(BaseTest):
         excluded = sample_strings(20, 5000)
 
         # Add only the included strings
-        self.bloom.extend(included)
-        self.assertEqual(len(included), len(self.bloom.contains(included)))
+        self.bloom.update(included)
+        self.assertEqual(len(included), len(self.bloom.intersection(included)))
 
-        false_positives = self.bloom.contains(excluded)
+        false_positives = self.bloom.intersection(excluded)
         false_rate = float(len(false_positives)) / len(excluded)
+        # print('FP: {0}, FR: {1:.3f}, EXPECTED: {2:.3f}'.format(false_positives, false_rate, 0.00001))
         self.assertLess(
             false_rate, 0.00001, 'False positive error rate exceeded!')
 
         # We also need to know that we can access all the keys we need
         self.assertEqual(self.bloom.keys(),
-                         ['pyreBloomTesting.0', 'pyreBloomTesting.1'])
+                         [b'pyreBloomTesting.0', b'pyreBloomTesting.1'])
 
 
 class Accuracytest(BaseTest):
@@ -143,10 +144,10 @@ class Accuracytest(BaseTest):
         excluded = sample_strings(20, 5000)
 
         # Add only the included strings
-        self.bloom.extend(included)
-        self.assertTrue(len(included) == len(self.bloom.contains(included)))
+        self.bloom.update(included)
+        self.assertTrue(len(included) == len(self.bloom.intersection(included)))
 
-        false_positives = self.bloom.contains(excluded)
+        false_positives = self.bloom.intersection(excluded)
         false_rate = float(len(false_positives)) / len(excluded)
         self.assertLess(
             false_rate, 0.1, 'False positive error rate exceeded!')
